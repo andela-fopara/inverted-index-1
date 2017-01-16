a4002f2bae6e4fd90d170d877fdba16918975e83
elahsoft <febechukwu.opara@andela.com>
2017-01-16 18:21:47 +0100

    gulp file rewritten in es6 standard and package.json updated for babel transpiling #development

--- gulpfile1.js
+++ gulpfile1.js
@@ -1,29 +1,29 @@
 //instruct node to look into the node_modules for the package gulp
-//var gulp = require('gulp');
-import gulp from 'gulp';
+var gulp = require('gulp');
+//import gulp from 'gulp';
 
 //add the  gulp-connect dependency
-//var connect = require('gulp-connect');
-import connect from 'gulp-connect';
+var connect = require('gulp-connect');
+//import connect from 'gulp-connect';
 
 //add the  open dependency require statement
-//var opens = require('gulp-open');
-import opens from 'gulp-open';
+var opens = require('gulp-open');
+//import opens from 'gulp-open';
 
 //add the jasmine dependency to actually run the jasmine test suites in .spec files
-//var jasmines = require('gulp-jasmine-livereload-task');
-import jasmines from 'gulp-jasmine-livereload-task';
+var jasmines = require('gulp-jasmine-livereload-task');
+//import jasmines from 'gulp-jasmine-livereload-task';
 //add dependency for opening
 
 //we start writing our gulp task
 //test with hello
-gulp.task('hello', () => {
+gulp.task('hello', function(){
 		//statements for the task goes here
 		console.log('Hello Zell');
 	});
 
 //the connect task to create a webserver running on http://localhost:8001
-gulp.task('connect', () => {
+gulp.task('connect', function(){
     connect.server({
         root: 'src',
         port: 8004,
@@ -32,13 +32,13 @@ gulp.task('connect', () => {
 });
 
 //adds task to open the web browser at the webserver
-gulp.task('open', () => {
+gulp.task('open', function(){
 	gulp.src('src/index.html')
 	.pipe(opens({uri: 'http://localhost:8004/'}));
 });
 
 //adds task to watch the filesystem and rebuild the project when a change is detected
-gulp.task('watch',() => {
+gulp.task('watch',function(){
 	gulp.watch('./src/*.html',['html']);
 	gulp.watch('./src/**/*.css',['css']);
 	gulp.watch('./src/**/*.js',['js']);
@@ -46,7 +46,7 @@ gulp.task('watch',() => {
 
 //adds a task to refresh our page automatically when there is a html file file edit
 //remember to add a piped connect.reload() to the css task and any other build task present in our project
-gulp.task('html', () => {
+gulp.task('html', function(){
 	gulp.src('./src/*.html')
 	.pipe(gulp.dest('./dist'))
 	.pipe(connect.reload());
@@ -54,7 +54,7 @@ gulp.task('html', () => {
 
 //adds a task to refresh our page automatically when there is a html file file edit
 //remember to add a piped connect.reload() to the css task and any other build task present in our project
-gulp.task('css', () => {
+gulp.task('css', function(){
 	gulp.src('./src/css/*.css')
 	.pipe(gulp.dest('./dist'))
 	.pipe(connect.reload());
@@ -62,7 +62,7 @@ gulp.task('css', () => {
 
 //adds a task to refresh our page automatically when there is a html file file edit
 //remember to add a piped connect.reload() to the css task and any other build task present in our project
-gulp.task('js', () => {
+gulp.task('js', function(){
 	 gulp.src('./src/js/*.js')
 	.pipe(gulp.dest('./dist'))
 	.pipe(connect.reload());
@@ -86,4 +86,5 @@ gulp.task('default', jasmines({
 
 //enables us to run the gulp test with just a gulp command
 gulp.task('default', ['connect','open','watch','html','css','js']);
+
 
